0dc9f15d908a76f332da56d9d6261a09af2bb901
diirtydog <andrew.derek.wales@gmail.com>
2022-04-18 10:06:58 -0500

    added some more of the cart logic

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { useStoreContext } from '../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../utils/actions';
+import {
+  UPDATE_PRODUCTS,
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  ADD_TO_CART,
+} from '../utils/actions';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
+import Cart from '../components/Cart';
 
 import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
@@ -30,6 +36,13 @@ function Detail() {
     }
   }, [products, data, dispatch, id]);
 
+  const addToCart = () => {
+    dispatch({
+      type: ADD_TO_CART,
+      product: { ...currentProduct, purchaseQuantity: 1 }
+    });
+  };
+
   return (
     <>
       {currentProduct ? (
@@ -42,7 +55,7 @@ function Detail() {
 
           <p>
             <strong>Price:</strong>${currentProduct.price}{' '}
-            <button>Add to Cart</button>
+            <button onClick={addToCart}>Add to Cart</button>
             <button>Remove from Cart</button>
           </p>
 
@@ -53,6 +66,7 @@ function Detail() {
         </div>
       ) : null}
       {loading ? <img src={spinner} alt="loading" /> : null}
+      <Cart />
     </>
   );
 }
